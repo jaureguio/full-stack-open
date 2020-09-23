0f28442de585dcdeb295885ae0631ecf27e7f8d7
Oscar <oscarjaureguia@gmail.com>
2020-09-23 09:34:18 -0400

    refactor(part1/courseinfo): solution 1.4, declare an unique parts array variable

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,56 +5,48 @@ const Header = ({ course }) => <h1>{course}</h1>
 
 const Part = ({ info }) => <p>{info.name} {info.exercises}</p>
 
-const Content = ({
-  part1,
-  part2,
-  part3,
-}) => (
-  <>
-    <Part info={part1} />
-    <Part info={part2} />
-    <Part info={part3} />
-  </>
-)
+const Content = ({ parts }) => {
+  const [part1, part2, part3] = parts
+  return (
+    <>
+      <Part info={part1} />
+      <Part info={part2} />
+      <Part info={part3} />
+    </>
+  )
+}
 
-const Total = ({ 
-  exercises1, 
-  exercises2, 
-  exercises3 
-}) => (
-  <p>
-    Number of exercises {exercises1 + exercises2 + exercises3}
-  </p>
-)
+const Total = ({ parts }) => {
+  const [part1, part2, part3] = parts
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total 
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
