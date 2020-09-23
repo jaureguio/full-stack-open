9ad5c12ca04be90ee49903f01ee995ae7cb7ce2d
Oscar <oscarjaureguia@gmail.com>
2020-09-23 09:23:31 -0400

    refactor(part1/courseinfo): solution 1.3, use objects for part variables declarations

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom'
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+const Part = ({ info }) => <p>{info.name} {info.exercises}</p>
 
 const Content = ({
   part1,
   part2,
   part3,
-  exercises1,
-  exercises2,
-  exercises3
 }) => (
   <>
-    <Part name={part1} exercises={exercises1} />
-    <Part name={part2} exercises={exercises2} />
-    <Part name={part3} exercises={exercises3} />
+    <Part info={part1} />
+    <Part info={part2} />
+    <Part info={part3} />
   </>
 )
 
@@ -32,28 +29,31 @@ const Total = ({
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   
   return (
     <div>
       <Header course={course} />
       <Content 
         part1={part1}
-        exercises1={exercises1}
         part2={part2}
-        exercises2={exercises2}
         part3={part3}
-        exercises3={exercises3}
       />
       <Total 
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </div>
   )
