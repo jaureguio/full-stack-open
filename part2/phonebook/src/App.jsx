eec8b2cabf684785d4e0f90bf79f99c768cf3902
Oscar <oscarjaureguia@gmail.com>
2020-09-24 09:11:51 -0400

    feat(part2/phonebook): solution 2.7-8, include contact number and avoid adding duplicated names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { 
+      name: 'Arto Hellas',
+      contact: '040-1234567', 
+    }
+  ]);
   const [newName, setNewName] = useState("");
+  const [contact, setContact] = useState();
 
-  const handleChange = event => setNewName(event.target.value)
+  const handleChange = (setState) => (event) => setState(event.target.value)
   
   const handleSubmit = event => {
     event.preventDefault()
 
+    const isAdded = persons.some(({ name }) => name === newName)
+
+    if(!newName.trim()) {
+      return
+    } else if(isAdded) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     setPersons(persons => ([
       ...persons,
-      { name: newName },
+      { 
+        name: newName,
+        contact
+      },
     ]))
+
+    setNewName('')
+    setContact('')
   }
 
   return (
@@ -20,15 +41,26 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: 
+          <input 
+            value={newName} 
+            onChange={handleChange(setNewName)} 
+          />
+        </div>
+        <div>
+          number: 
+          <input 
+            value={contact} 
+            onChange={handleChange(setContact)} 
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name }, idx) =>
-        <p key={name+idx}>{name}</p>
+      {persons.map(({ name, contact }, idx) =>
+        <p key={name+idx}>{name} {contact}</p>
       )}
     </div>
   );
