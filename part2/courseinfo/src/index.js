ba3b1f0876191301412d93c7116c76645e6d6721
Oscar <oscarjaureguia@gmail.com>
2020-09-23 22:07:52 -0400

    feat(part2): add part2 base files

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ info }) => <p>{info.name} {info.exercises}</p>
+
+const Content = ({ parts }) => {
+  const [part1, part2, part3] = parts
+  return (
+    <>
+      <Part info={part1} />
+      <Part info={part2} />
+      <Part info={part3} />
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  const [part1, part2, part3] = parts
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
