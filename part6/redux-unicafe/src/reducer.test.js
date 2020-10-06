575ace0aafb9bee1eb2dc38c8373097584bea0b2
Oscar <oscarjaureguia@gmail.com>
2020-10-06 12:06:16 -0400

    feat(part6/redux-unicafe): add unicafe redux-based functionality with basic tests

---
+++ part6/redux-unicafe/src/reducer.test.js
@@ -0,0 +1,78 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('state is reset to initialState', () => {
+    const state = {
+      good: 3,
+      ok: 2,
+      bad: 0
+    }
+
+    const action = {
+      type: 'ZERO'
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+})
